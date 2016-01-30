c886bb10349d64f6e9f79aa313b2fc7b19fb6bad
pascualy <pascualy@umich.edu>
2016-01-30 17:14:33 -0500

    new

--- launch/base.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#configure xbox controller
-joystick=$(ls /dev/input/js*) || echo "Joystick not found. Please connect usb."; # output error if no js found
-sudo chmod a+rw $joystick;
-
-#run launch file
-roslaunch ~/catkin_ws/launch/base.launch
