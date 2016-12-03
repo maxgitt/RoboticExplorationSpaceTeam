5bea581c9bf9a7c0e5210ec46a3a61e75b104314
Gabriel Pascualy <pascualy@umich.edu>
2016-12-03 17:21:13 +0000

    add source for necessary environemtn variables

--- vagrant/catkinConfig.sh
+++ vagrant/catkinConfig.sh
@@ -1,4 +1,7 @@
-source /opt/ros/kinetic/setup.bash
+echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
+echo "source /home/ubuntu/catkin_ws/devel/setup.bash" >> ~/.bashrc
+echo "source /home/ubuntu/catkin_ws/src/rover_gazebo/setup.sh" >> ~/.bashrc
+source ~/.bashrc
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
 catkin_init_workspace
