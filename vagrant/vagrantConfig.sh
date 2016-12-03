4a363e3b80b20b2f4e5492bb5e03afdb595a42e8
Gabriel Pascualy <pascualy@umich.edu>
2016-12-03 17:48:08 +0000

    add sublime installation

--- vagrant/vagrantConfig.sh
+++ vagrant/vagrantConfig.sh
@@ -1,5 +1,6 @@
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
 sudo apt-get update
-sudo apt-get install -y ubuntu-desktop virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
+sudo apt-get install -y ubuntu-desktop virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11 sublime-text-installer
 sudo VBoxClient --clipboard
 sudo VBoxClient --draganddrop
 sudo VBoxClient --display
