0be098ad59f26f8ceaf193da6165734d208d9699
Gabriel Pascualy <pascualy@umich.edu>
2016-12-03 14:38:02 +0000

    add config file for catkin

---
+++ vagrant/catkinConfig.sh
@@ -0,0 +1,8 @@
+source /opt/ros/kinetic/setup.bash
+mkdir -p ~/catkin_ws/src
+cd ~/catkin_ws/src
+catkin_init_workspace
+cd ~/catkin_ws/
+catkin_make
+source devel/setup.bash
+echo $ROS_PACKAGE_PATH
