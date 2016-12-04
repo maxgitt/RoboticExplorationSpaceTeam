a2b8aa840ecc47fda9b0136972160bbc6342bf30
Kishore-B-Rao <kishbrao@umich.edu>
2016-12-04 12:49:49 -0500

    Updated catkin config

--- vagrant/catkinConfig.sh
+++ vagrant/catkinConfig.sh
@@ -1,11 +1,11 @@
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
-echo "source /home/ubuntu/catkin_ws/devel/setup.bash" >> ~/.bashrc
-echo "source /home/ubuntu/catkin_ws/src/rover_gazebo/setup.sh" >> ~/.bashrc
 source ~/.bashrc
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
 catkin_init_workspace
 cd ~/catkin_ws/
 catkin_make
-source devel/setup.bash
 echo $ROS_PACKAGE_PATH
+echo "source /home/ubuntu/catkin_ws/devel/setup.bash" >> ~/.bashrc
+echo "source /home/ubuntu/catkin_ws/src/rover_gazebo/setup.sh" >> ~/.bashrc
+source devel/setup.bash
