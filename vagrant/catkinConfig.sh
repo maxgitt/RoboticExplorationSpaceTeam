2931191e771bb3d88ead6953ad159cb4cef504c4
Gabriel Pascualy <pascualy@umich.edu>
2016-12-04 13:52:40 -0500

    remove making new catkin workspac

--- vagrant/catkinConfig.sh
+++ vagrant/catkinConfig.sh
@@ -1,9 +1,7 @@
 echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
-mkdir -p ~/catkin_ws/src
+mkdir ~/catkin_ws/src
 cd ~/catkin_ws/src
-/opt/ros/kinetic/bin/catkin_init_workspace
-cd ~/catkin_ws/
 /opt/ros/kinetic/bin/catkin_make
 echo $ROS_PACKAGE_PATH
 echo "source /home/ubuntu/catkin_ws/devel/setup.bash" >> ~/.bashrc
