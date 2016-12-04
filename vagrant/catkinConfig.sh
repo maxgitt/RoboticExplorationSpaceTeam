12f8bf2e0b0d6c0e0a986a022f8893581b139c39
Gabriel Pascualy <pascualy@umich.edu>
2016-12-04 13:02:26 -0500

    adding full path for catkin commands

--- vagrant/catkinConfig.sh
+++ vagrant/catkinConfig.sh
@@ -2,9 +2,9 @@ echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
-catkin_init_workspace
+/opt/ros/kinetic/bin/catkin_init_workspace
 cd ~/catkin_ws/
-catkin_make
+/opt/ros/kinetic/bin/catkin_make
 echo $ROS_PACKAGE_PATH
 echo "source /home/ubuntu/catkin_ws/devel/setup.bash" >> ~/.bashrc
 echo "source /home/ubuntu/catkin_ws/src/rover_gazebo/setup.sh" >> ~/.bashrc
