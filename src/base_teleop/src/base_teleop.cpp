e1d6e2b5de0ee4040516f4689cd19ee9c9ea7f84
Gabriel Pascualy <pascualy@umich.edu>
2017-01-20 14:48:09 -0500

    simming working

--- src/base_teleop/src/base_teleop.cpp
+++ src/base_teleop/src/base_teleop.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
+#include "rover_controls/Excavation.h"
 #include <iostream>
 #include <cmath>
 
@@ -23,8 +24,17 @@ private:
   int left;
   double l_scale, a_scale;
   ros::Publisher vel_pub;
+  ros::Publisher excv_pub;
   ros::Subscriber joy_sub;
+  
+  // 0 - disable
+  // 1 - 
+  bool disabled = true;
 
+  int excv_tog = 0;
+  int act_tog = 0;
+  int actL   = 0;
+  int actR   = 0;
 };
 
 
@@ -38,13 +48,38 @@ TeleopREST::TeleopREST():
   nh_.param("scale_angular", a_scale, a_scale);
   nh_.param("scale_linear", l_scale, l_scale);
 
+  excv_pub = nh_.advertise<rover_controls::Excavation>("cmd_excv", 1);
   vel_pub = nh_.advertise<geometry_msgs::Twist>("cmd_vel", 1);
   joy_sub = nh_.subscribe<sensor_msgs::Joy>("joy", 10,&TeleopREST::joyCallback, this);
 
 }
 
+bool TeleopREST::is_disabled(){
+  if(disabled == true) {
+      geometry_msgs::Twist vel;
+    vel.linear.x = 0;
+    vel.angular.z = 0;
+    vel_pub.publish(vel);
+    //add disabling code for excavation  
+    act_tog = 0;
+      rover_controls::Excavation excv;
+    excv.left_actuator  = 0;
+    excv.right_actuator = 0;
+    excv.excavator_speed = 0;
+    return true;
+  }
+  else return false;
+}
+
 void TeleopREST::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
 {
+ cout << "disabled" << disabled << endl;
+  if(joy->buttons[7] == 1){
+  if(disabled) disabled = false;
+  else disabled = true;
+  }
+  if(is_disabled()) return;
+
   geometry_msgs::Twist vel;  
   double vl = joy->axes[left];
   double vr = joy->axes[right];
@@ -64,25 +99,63 @@ void TeleopREST::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
   {
     vr = sqrt(vr);
   }
-	
-	if( vl == vr){
-		//forward/backward
-		vel.linear.x = vl; // or vr
-		vel.angular.z = 0;
-	}
-	else if( (-1 * vl) == vr){
-		//turning
-		vel.angular.z = 2*vr/ROBOT_WIDTH;
-		vel.linear.x = 0;
-	}
-	else{
-		//moving doing arcs
-		vel.angular.z = (vr - vl)/ROBOT_WIDTH;
-		vel.linear.x = vr - vel.angular.z * ROBOT_WIDTH / 2.0;
-	}
+  
+  if( vl == vr){
+    //forward/backward
+    vel.linear.x = vl; // or vr
+    vel.angular.z = 0;
+  }
+  else if( (-1 * vl) == vr){
+    //turning
+    vel.angular.z = 2*vr/ROBOT_WIDTH;
+    vel.linear.x = 0;
+  }
+  else{
+    //moving doing arcs
+    vel.angular.z = (vr - vl)/ROBOT_WIDTH;
+    vel.linear.x = vr - vel.angular.z * ROBOT_WIDTH / 2.0;
+  }
   vel_pub.publish(vel);
 
+  if(joy->buttons[0] == 1 && joy->buttons[3] == 1 && !disabled){
+  cout << "Actuators Disabled: Only toggle one button" << endl;
+  act_tog = 0;  
+  }
+  else if(joy->buttons[3] == 1 && !disabled){
+  cout << "Actuators set to EXTEND" << endl;  
+  act_tog = 1;  
+  }
+  else if(joy->buttons[0] == 1 && !disabled){
+  cout << "Actuators set to RETRACT" << endl; 
+  act_tog = -1;   
+  }
+  else{
+  cout << "Actuators set to " << act_tog << endl; 
+  }
 
+  if(joy->buttons[1] == 1 && joy->buttons[2] == 1 && !disabled){
+  cout << "Excavator Disabled: Only toggle one button" << endl;
+  excv_tog = 0; 
+  }
+  else if(joy->buttons[1] == 1 && !disabled){
+  cout << "Excavator set to FORWARD" << endl; 
+  excv_tog = 1; 
+  }
+  else if(joy->buttons[2] == 1 && !disabled){
+  cout << "Excavator set to REVERSE" << endl; 
+  excv_tog = -1;    
+  }
+  else{
+  cout << "Excavator set to " << excv_tog << endl;  
+  }
+
+
+  rover_controls::Excavation excv;
+  excv.manual     = true;
+  excv.left_actuator    = act_tog * abs(joy->buttons[4]);
+  excv.right_actuator   = act_tog * abs(joy->buttons[5]);
+  excv.excavator_speed  = excv_tog * (joy->axes[5] - 1) / 2;
+  excv_pub.publish(excv);
 }
 
 
--- src/rover_gazebo/plugins/EncoderPlugin/build/CMakeFiles/rover_encoder_generate_messages_nodejs.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/rover_encoder_generate_messages_nodejs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
