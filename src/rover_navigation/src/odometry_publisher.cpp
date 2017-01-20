cdbe76c1aa22f3155d77f570155bdeec9160ba59
Gabriel Pascualy <pascualy@umich.edu>
2017-01-20 15:29:44 -0500

    update

---
+++ src/rover_navigation/src/odometry_publisher.cpp
@@ -0,0 +1,74 @@
+#include <ros/ros.h>
+#include <tf/transform_broadcaster.h>
+#include <nav_msgs/Odometry.h>
+
+
+class OdometryPublisher
+{
+public:
+  OdometryPublisher();
+
+private:
+  void odomCallback(const nav_msgs::Odometry::ConstPtr& odom);
+  ros::NodeHandle nh_;
+  tf::TransformBroadcaster tf_broadcaster;
+  ros::Publisher odom_pub;
+  ros::Subscriber odom_sub;
+    
+  ros::Time current_time;
+  ros::Time last_time;
+};
+
+
+OdometryPublisher::OdometryPublisher()
+{
+  odom_pub = nh_.advertise<nav_msgs::Odometry>("odom", 50);
+
+  odom_sub = nh_.subscribe<nav_msgs::Odometry>("/odom", 1, &OdometryPublisher::odomCallback, this);
+
+  current_time = ros::Time::now();
+  last_time = ros::Time::now();
+}
+
+void OdometryPublisher::odomCallback(const nav_msgs::Odometry::ConstPtr& old_odom)
+{
+	nav_msgs::Odometry odom = *old_odom;
+
+  tf_broadcaster.sendTransform(
+        tf::StampedTransform(
+          tf::Transform(tf::Quaternion(0, 0, 0, 1), tf::Vector3(0.0, 0.0, 0.0)),
+          ros::Time::now(),"map", "odom"));
+
+  //since all odometry is 6DOF we'll need a quaternion created from yaw
+  geometry_msgs::Quaternion odom_quat = odom.pose.pose.orientation;
+
+  //first, we'll publish the transform over tf
+  geometry_msgs::TransformStamped odom_trans;
+  odom_trans.header.stamp = ros::Time::now();
+  odom_trans.header.frame_id = "odom";
+  odom_trans.child_frame_id = "base_link";
+
+  odom_trans.transform.translation.x = odom.pose.pose.position.x;
+  odom_trans.transform.translation.y = odom.pose.pose.position.y;
+  odom_trans.transform.translation.z = 0.0;
+  odom_trans.transform.rotation = odom_quat;
+
+  //send the transform
+  tf_broadcaster.sendTransform(odom_trans);
+
+
+  //next, we'll publish the odometry message over ROS
+  odom.header.frame_id = "odom";
+
+  //publish the message
+  odom_pub.publish(odom);
+
+}
+
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "odometry_publisher");
+  OdometryPublisher odom;
+  ros::spin();
+}
---
+++ staging/minibase_teleop/src/base_teleop.cpp
@@ -0,0 +1,97 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <sensor_msgs/Joy.h>
+#include <iostream>
+#include <cmath>
+
+using std::cout; using std::endl;
+//rover physical properties
+#define ROBOT_WIDTH .75 //meters
+#define WHEEL_DIAMETER .5 //meters
+
+class TeleopREST
+{
+public:
+  TeleopREST();
+
+private:
+  void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
+  bool is_disabled();
+  ros::NodeHandle nh_;
+
+  int right;
+  int left;
+  double l_scale, a_scale;
+  ros::Publisher vel_pub;
+  ros::Publisher vel_pub_gaz;
+
+  ros::Subscriber joy_sub;
+
+};
+
+
+TeleopREST::TeleopREST():
+  right(1),
+  left(4)
+{
+
+  nh_.param("axis_right", right, right);
+  nh_.param("axis_left", left, left);
+  nh_.param("scale_angular", a_scale, a_scale);
+  nh_.param("scale_linear", l_scale, l_scale);
+  // vel_pub_gaz = nh_.advertise<geometry_msgs::Twist>("/rover/cmd_vel", 1);
+  vel_pub = nh_.advertise<geometry_msgs::Twist>("/rover/cmd_vel", 1);
+  joy_sub = nh_.subscribe<sensor_msgs::Joy>("joy", 10,&TeleopREST::joyCallback, this);
+
+}
+
+void TeleopREST::joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
+{
+  geometry_msgs::Twist vel;  
+  double vl = joy->axes[left];
+  double vr = joy->axes[right];
+  if(vl < 0)
+  {
+    vl = -1 * sqrt((vl* -1));
+  }
+  else
+  {
+    vl = sqrt(vl);
+  }
+  if(vr < 0)
+  {
+    vr = -1 * sqrt((vr* -1));
+  }
+  else
+  {
+    vr = sqrt(vr);
+  }
+	
+	if( vl == vr){
+		//forward/backward
+		vel.linear.x = vl; // or vr
+		vel.angular.z = 0;
+	}
+	else if( (-1 * vl) == vr){
+		//turning
+		vel.angular.z = 2*vr/ROBOT_WIDTH;
+		vel.linear.x = 0;
+	}
+	else{
+		//moving doing arcs
+		vel.angular.z = (vr - vl)/ROBOT_WIDTH;
+		vel.linear.x = vr - vel.angular.z * ROBOT_WIDTH / 2.0;
+	}
+  vel_pub.publish(vel);
+  // vel_pub_gaz.publish(vel);
+
+}
+
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "teleop_REST");
+  TeleopREST teleop_rest;
+
+  ros::spin();
+}
