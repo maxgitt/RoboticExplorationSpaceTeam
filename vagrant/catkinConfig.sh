675864e8749f8caf9f7382dce6d2c0ab3a8ff3fa
Gabriel Pascualy <pascualy@umich.edu>
2017-02-07 18:39:15 -0500

    update configs

--- vagrant/catkinConfig.sh
+++ vagrant/catkinConfig.sh
@@ -2,9 +2,9 @@ echo "source /opt/ros/kinetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 mkdir ~/catkin_ws
 mkdir ~/catkin_ws/src
-cd ~/catkin_ws/src
+cd ~/catkin_ws
 /opt/ros/kinetic/bin/catkin_make
-echo $ROS_PACKAGE_PATH
-echo "source /home/ubuntu/catkin_ws/devel/setup.bash" >> ~/.bashrc
-echo "source /home/ubuntu/catkin_ws/src/rover_gazebo/setup.sh" >> ~/.bashrc
+echo "source /home/"$USER"/catkin_ws/devel/setup.bash" >> ~/.bashrc
+echo "source /home/"$USER"/catkin_ws/src/rover_gazebo/setup.sh" >> ~/.bashrc
+cd ~/catkin_ws
 source devel/setup.bash
--- vagrant/gazeboConfig.sh
+++ vagrant/gazeboConfig.sh
@@ -1,9 +1,6 @@
-
 # download and install gazebo
 curl -ssL http://get.gazebosim.org | sh
 
-echo "source /usr/share/gazebo/setup.sh
-source ~/catkin_ws/src/rover_gazebo/setup.sh
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
-" >> ~/.bashrc
+echo "source /usr/share/gazebo/setup.sh" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib" >> ~/.bashrc
 
