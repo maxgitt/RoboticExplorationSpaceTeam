103088ea263991c835ceaa6c72812493185d53c2
Kishore B. Rao <kishbrao@umich.edu>
2017-03-11 15:53:05 -0500

    This shit works

--- src/rover_navigation/src/odometry_publisher.cpp
+++ src/rover_navigation/src/odometry_publisher.cpp
@@ -22,7 +22,7 @@ private:
 
 OdometryPublisher::OdometryPublisher()
 {
-  odom_pub = nh_.advertise<nav_msgs::Odometry>("odom", 50);
+  //odom_pub = nh_.advertise<nav_msgs::Odometry>("odom", 50);
 
   odom_sub = nh_.subscribe<nav_msgs::Odometry>("/gazebo/odom", 1, &OdometryPublisher::odomCallback, this);
 
