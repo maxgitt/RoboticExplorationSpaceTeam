411592aabd828867109557b7631e7789d34111bf
Max Gittelman <maxgitt@umich.edu>
2017-03-15 18:32:28 +0000

    added tc for real data, window_size =1, thresh = 500 to 700, seems to be optimal. Needs additional testing, esp for oscillations

--- src/urg_node_filtered/src/unit_test.cpp
+++ src/urg_node_filtered/src/unit_test.cpp
@@ -2,7 +2,9 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <string>
 #include <urg_node/urg_c_wrapper.h>
+#include <ros/package.h>
 
 #define NUM_STEPS 1080
 
@@ -54,23 +56,33 @@ void test_ten_stepping_rising_edges_1() {
 	assert(edges.size() == 9);
 }
 
+/* Uses real-test data from checkerboard, 10 edges, 2 nonreal */
 void test_eight_edges_1() {
-	// NOT READING IN FILE CORRECTLY##############################
 
-	int window_size = 5;
-	int threshold_delta = 400;
+	int window_size = 1;
+	int threshold_delta = 500;
 
-	ifstream infile("test_data.txt");
+	// Create path to test data
+	string path = ros::package::getPath("urg_node_filtered");
+	path += "/src/test_data.txt";
+
+	ifstream infile(path.c_str());
 	int intensity_val, idx = 0;
 	vector<float> intensities(NUM_STEPS);
 
 	while (infile >> intensity_val) {
 		intensities[idx++] = intensity_val;
-		cout << "Intensity at idx " << idx - 1 << ": " << intensity_val << "\n";
+		//cout << "Intensity at idx " << idx - 1 << ": " << intensity_val << "\n";
 	}
 
 	vector<int> edges = determine_intensity_edges(intensities, NUM_STEPS, window_size, threshold_delta);
-	assert(edges.size() == 8);
+
+	// for (int j = 0; j < edges.size(); ++j) {
+	// 	cout << edges[j] << " ";
+	// }
+	// cout << endl;
+	// cout << "Edges Detected: " << edges.size()  << "\n";
+	assert(edges.size() == 10);
 
 }
 
